88770f74b003c19fd21a7e8fee830531b013fa35
lPdFUzZT <thatelitemaili33t@gmail.com>
2021-07-17 18:48:41 +0300

    Fixed express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname + '/public')));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
